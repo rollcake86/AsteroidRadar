74ef307383ff8b101330db9e99469f83f51f8b33
rollcake86 <ikinye86@gmail.com>
2023-01-06 21:34:02 +0900

    - comment 추가

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -5,7 +5,6 @@ import android.view.*
 import android.widget.ImageView
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -15,7 +14,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.R
-import com.udacity.asteroidradar.database.AsteroidDatabase
 import com.udacity.asteroidradar.databinding.AsteroidItemBinding
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 import timber.log.Timber
@@ -46,7 +44,7 @@ class MainFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val binding = FragmentMainBinding.inflate(inflater)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
@@ -55,9 +53,10 @@ class MainFragment : Fragment() {
             this.findNavController().navigate(MainFragmentDirections.actionShowDetail(it))
         })
 
-        viewModel.todayImage.observe(viewLifecycleOwner, Observer {
-            Picasso.with(context).load(it.url).into(binding.root.findViewById<ImageView>(R.id.activity_main_image_of_the_day))
-        })
+        viewModel.todayImage.observe(viewLifecycleOwner) {
+            Picasso.with(context).load(it.url)
+                .into(binding.root.findViewById<ImageView>(R.id.activity_main_image_of_the_day))
+        }
         setHasOptionsMenu(true)
         binding.root.findViewById<RecyclerView>(R.id.asteroid_recycler).apply {
             layoutManager = LinearLayoutManager(context)
--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,9 +1,7 @@
 package com.udacity.asteroidradar.main
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.*
-import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.PictureOfDay
 import com.udacity.asteroidradar.database.AsteroidDatabase
 import com.udacity.asteroidradar.network.NasaApi
